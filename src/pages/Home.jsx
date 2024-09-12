82d0eac7a454c04b6e47812d11ef4110189cd000
itsdreamy <syifasaepulbachri@gmail.com>
2024-09-12 11:57:34 +0700

    Sidebar UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+import Sidebar from '../components/sidebar';
+import Navbar from '../components/navbar';
+function Home(){
+    return (
+        <div>
+            <Sidebar />         
+        </div>
+    );
+}
+
+export default Home;
