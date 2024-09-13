e09121f12ecc78ecbae5c83ec71a75f96b02eb34
itsdreamy <syifasaepulbachri@gmail.com>
2024-09-13 12:33:29 +0700

    Dashboard adding front-end

--- help-me/src/App.js
+++ help-me/src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import './App.css';
-
+import { ColorModeContext } from "./theme";
+import {useMode} from "./theme";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Form from "./scenes/dashboard";
+// import Bar from "./scenes/dashboard";
+// import Pie from "./scenes/dashboard";
+// import FAQ from "./scenes/dashboard";
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <div className="app">
+          <Sidebar />
+              <main className="content">
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  {/* <Route path="/team" element={<Team />} />
+                  <Route path="/invoices" element={<Invoices />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/form" element={<Form />} />
+                  <Route path="/pie" element={<Pie />} />
+                  <Route path="/faq" element={<FAQ />} /> */}
+                </Routes>
+              </main>   
+        </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ help-me/src/scenes/global/Sidebar.jsx
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import "react-pro-sidebar/dist/css/styles.css";
+import { tokens } from "../../theme";
+import DoorbellIcon from '@mui/icons-material/Doorbell';
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import PrivacyTipIcon from '@mui/icons-material/PrivacyTipOutlined';
+import CarCrashIcon from '@mui/icons-material/CarCrashOutlined';
+import ElectricalServicesIcon from '@mui/icons-material/ElectricalServices';
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import LogoutIcon from '@mui/icons-material/LogoutOutlined';
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" sx={{ fontWeight: "bold" }} color={colors.grey[100]}>
+                  HelpMe !
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/rar.jpeg`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Fadil
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  Administrator
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<DashboardOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Users
+            </Typography>
+            <Item
+              title="Kelola Mitra"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Kelola Pengguna"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Kategori Bantuan
+            </Typography>
+            <Item
+              title="Bantuan Serabutan"
+              to="/form"
+              icon={<PrivacyTipIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Bantuan Kendaraan"
+              to="/calendar"
+              icon={<CarCrashIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Bantuan Elektronik"
+              to="/faq"
+              icon={<ElectricalServicesIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Bantuan Rumah"
+              to="/faq"
+              icon={<DoorbellIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 0 5px 20px" }}
+            >
+              Charts
+            </Typography>
+            <Item
+              title="Bar Chart"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Pie Chart"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+          <Box sx={{ m: "6px 0 15px 20px"}}>
+          <Item
+              title="Logout"
+              to="/bar"
+              icon={<LogoutIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
+};
+
+export default Sidebar;
