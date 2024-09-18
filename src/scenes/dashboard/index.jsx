bd67e4fb3feba62c8f7786dc0989837f65254732
KnowRise <rifaasiraajuddin.123@gmail.com>
2024-09-18 14:13:28 +0700

    -

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -18,22 +18,6 @@ const Dashboard = () => {
             <Box display="flex" justifyContent="space-between" alignItems="center">
             <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
             
-
-            <Box>
-                <Button
-                sx={{
-                    backgroundColor: colors.blueAccent[700],
-                    color: colors.grey[100],
-                    fontSize: "14px",
-                    fontWeight: "bold",
-                    padding: "10px 20px",
-                    borderRadius: "10px",
-                }}
-                >
-                <DownloadOutlinedIcon sx={{ mr: "10px" }} />
-                Download Reports
-                </Button>
-            </Box>
             </Box>
                 {/* GRID & CHARTS */}
                 <Box
