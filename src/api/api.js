6015a33abb81d5fccbe2a51f6209b94ae236976c
KnowRise <rifaasiraajuddin.123@gmail.com>
2024-09-30 15:33:56 +0700

    -

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://6300fdf05c913991db83d064627d305d.serveo.net/api/v1";
-export const BASE_URL = "https://6300fdf05c913991db83d064627d305d.serveo.net";
+export const API_URL = "https://874b2c3c8a37bc32d5d920ee50e8e625.serveo.net/api/v1";
+export const BASE_URL = "https://874b2c3c8a37bc32d5d920ee50e8e625.serveo.net";
