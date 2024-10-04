52cd93286f9ba4aebd26b63fda92e467eddcc4cf
itsdreamy <syifasaepulbachri@gmail.com>
2024-10-04 10:37:59 +0700

    dashboard fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://1d733ab2a45a9295602a16af51b731f2.serveo.net/api/v1";
-export const BASE_URL = "https://1d733ab2a45a9295602a16af51b731f2.serveo.net";
+export const API_URL = "https://001532a13c0e47d5456aa2bae8293335.serveo.net/api/v1";
+export const BASE_URL = "https://001532a13c0e47d5456aa2bae8293335.serveo.net";
